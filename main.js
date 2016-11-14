da021deaa586f9b4bae311f688eb05aa84918bf8
Dan Nguyen <Dan Nguyen>
2016-11-13 22:22:36 -0600

    added functions

--- main.js
+++ main.js
@@ -4,13 +4,19 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
- result = "Sorry, Try Again";
-} else if (cardOne === cardTwo) {
- result = "You found a match!"
-} else if (cardThree === cardFour) {
- result = "You found a match!"
-} else if (cardTwo === cardThree) {
- result = "Sorry, Try Again"
-} 
-alert(result);
+if (cardOne === cardTwo) {
+alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
+
+var gameboard = document.getElementById('game-board');
+
+function createBoard() {
+ for (var i=0; i<cards.length; i++) {
+var cardElement = document.createElement('div');
+cardElement.className ='card';
+board.appendChild(cardElement);
+}
+}
+createBoard();
