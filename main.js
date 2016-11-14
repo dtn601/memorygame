374f93e00aa5e6005a11b66ac5c596975d8c652b
Dan Nguyen <Dan Nguyen>
2016-11-13 18:37:43 -0600

    created matchingn logic

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+ result = "Sorry, Try Again";
+} else if (cardOne === cardTwo) {
+ result = "You found a match!"
+} else if (cardThree === cardFour) {
+ result = "You found a match!"
+} else if (cardTwo === cardThree) {
+ result = "Sorry, Try Again"
+} 
+alert(result);
